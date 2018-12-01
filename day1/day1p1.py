52ccaea8f01ad9ac080a0139e52ddc678c0ce360
Eric Darchis <darchis@pobox.com>
2018-12-01 12:36:22 +0100

    Day 1 Two different approaches

--- day1/day1p1.py
+++ day1/day1p1.py
@@ -1,7 +1,15 @@
 input_file = open("input.txt", "r")
 
+# Good: fast and uses nearly no memory, less good: good old procedural programming (I'd still go for this one)
 frequency = 0
 for shift in input_file:
-    frequency += shift
+    frequency += int(shift)
 
 print(frequency)
+
+input_file.seek(0)
+
+# The drawback of this method is that it creates an array in memory before summing it
+frequency2 = sum([int(shift) for shift in input_file])
+
+print(frequency2)
