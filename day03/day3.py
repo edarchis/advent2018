cf5e1a906c7dc8d8ddc5107eb29514890a01754b
Eric Darchis <darchis@pobox.com>
2018-12-27 22:47:04 +0100

    day03 part 2 still easy

--- day03/day3.py
+++ day03/day3.py
@@ -40,15 +40,24 @@ def show_overlaps(area):
             print("cell", key[0], key[1], "is assigned to", value)
 
 
+def get_overlaps(area):
+    return {k: v for k, v in area.items() if len(v) > 1}
+
+
+def not_overlapping(overlaps, claims):
+    return {cl.id for cl in claims} - {ov for k, v in overlaps.items() for ov in v}
+
+
 sample_area = {}
 sample_claims = load_file("sample.txt")
 for claim in sample_claims:
     assign_segment(sample_area, claim)
     print(claim)
 
-overlaps = {k: v for k, v in sample_area.items() if len(v) > 1}
-print(len(overlaps))
+sample_overlaps = get_overlaps(sample_area)
+print(len(sample_overlaps))
 show_overlaps(sample_area)
+print("not overlapping:", not_overlapping(sample_overlaps, sample_claims))
 
 actual_area = {}
 actual_claims = load_file("input.txt")
@@ -56,6 +65,6 @@ for claim in actual_claims:
     assign_segment(actual_area, claim)
     print(claim)
 
-overlaps = {k: v for k, v in actual_area.items() if len(v) > 1}
-print(len(overlaps))
-
+actual_overlaps = get_overlaps(actual_area)
+print(len(actual_overlaps))
+print("actual not overlapping:", not_overlapping(actual_overlaps, actual_claims))
